07c8965e3e67276d5ca1e7a7619e3d5abbb7ae42
yogesh1612 <yogesh1612@users.noreply.github.com>
2021-06-23 22:10:51 +0530

    .

--- qna_app.py
+++ qna_app.py
@@ -1,6 +1,6 @@
 import joblib
-import requests
 import streamlit as st
+import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding',False)
 st.title('QnA App')
 st.text('Ask questions relevant to Earnings call')
@@ -9,17 +9,33 @@ st.text('Ask questions relevant to Earnings call')
 def load_model():
     model = joblib.load('bert_qa_custom.joblib')
     return model
+@st.cache(allow_output_mutation=True)
+def load_data():
+  df = pd.read_csv('df.csv')
+  return df
 
-with st.spinner('Loading Model Into Memory...'):
+def main():
+  
+  with st.spinner('Loading Model Into Memory...'):
     model = load_model()
-
-text = st.text_input("Enter your questions here..")
-
-if text:
-    st.write("Response :")
+  text = st.text_input("Enter your questions here..")
+  n_rtn = st.sidebar.slider("Number of returned predictions",1,6,1)
+  st.markdown('Data used for model training')
+  st.write(load_data())
+  if text:
+    
     with st.spinner("Searching for answers.."):
-        prediction = model.predict(text)
-        st.write('answer: {}'.format(prediction[0]))
-        st.write('title: {}'.format(prediction[1]))
-        st.write('paragraph: {}'.format(prediction[2]))
-        
+        prediction = model.predict(text,n_predictions = n_rtn)
+
+    st.write("Response :")
+    for p in prediction:
+      st.write('--------------------------------------')
+      st.write('answer: {}'.format(p[0]))
+      st.write('title: {}'.format(p[1]))
+      st.write('paragraph: {}'.format(p[2]))
+      st.write('retriever_score_weight: {}'.format(round(p[3],3)))
+      st.write('--------------------------------------')
+
+
+if __name__=='__main__':
+  main()
